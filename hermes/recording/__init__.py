532eb5fe54180ef7d7b17316d2a35fde21e0c45c
vrajkher <vrajkher@gmail.com>
2026-09-09 23:41:07 +0530

    Export Graphiti work memory

--- hermes/recording/__init__.py
+++ hermes/recording/__init__.py
@@ -1,6 +1,7 @@
-"""Observable AI work recording and skill compilation utilities."""
+"""Observable AI work recording, Graphiti memory and skill compilation utilities."""
 
+from .graphiti_memory import GraphitiWorkMemory
 from .recorder import WorkRecorder
 from .skill_compiler import SkillCompiler
 
-__all__ = ["WorkRecorder", "SkillCompiler"]
+__all__ = ["WorkRecorder", "SkillCompiler", "GraphitiWorkMemory"]
