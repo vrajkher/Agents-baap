57c1b11011f26b9e25b7b8fa9b94224df85a5571
vrajkher <vrajkher@gmail.com>
2026-09-09 23:45:18 +0530

    Export automatic OpenAI work capture utilities

--- hermes/recording/__init__.py
+++ hermes/recording/__init__.py
@@ -1,7 +1,14 @@
 """Observable AI work recording, Graphiti memory and skill compilation utilities."""
 
 from .graphiti_memory import GraphitiWorkMemory
+from .openai_capture import OpenAITraceStore, install_openai_agents_capture
 from .recorder import WorkRecorder
 from .skill_compiler import SkillCompiler
 
-__all__ = ["WorkRecorder", "SkillCompiler", "GraphitiWorkMemory"]
+__all__ = [
+    "WorkRecorder",
+    "SkillCompiler",
+    "GraphitiWorkMemory",
+    "OpenAITraceStore",
+    "install_openai_agents_capture",
+]
